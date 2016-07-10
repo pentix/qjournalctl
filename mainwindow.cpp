af779f0690ab1f86f6e6a8017231d66d1261faa7
Patrick Eigensatz <patrick.eigensatz@gmail.com>
2016-07-10 13:51:21 +0200

    system boots displayed in a tableview in main window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,7 @@
 #include "aboutdialog.h"
 
 #include <QProcess>
+#include <iostream>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -13,7 +14,7 @@ MainWindow::MainWindow(QWidget *parent) :
 
     // Set Item Model
     itemModel = new QStandardItemModel(this);
-    ui->listView1->setModel(itemModel);
+    ui->tableView->setModel(itemModel);
 }
 
 MainWindow::~MainWindow()
@@ -33,10 +34,27 @@ void MainWindow::on_pushButton_2_clicked()
     QStringList lines = stdout.split("\n", QString::SkipEmptyParts);
 
 
+    QStandardItemModel *bootModel = new QStandardItemModel(30, 5, this);
+    bootModel->setHorizontalHeaderItem(0, new QStandardItem(QString("Boot No.")));
+    bootModel->setHorizontalHeaderItem(1, new QStandardItem(QString("Boot ID")));
+    bootModel->setHorizontalHeaderItem(2, new QStandardItem(QString("Day")));
+    bootModel->setHorizontalHeaderItem(3, new QStandardItem(QString("Date")));
+    bootModel->setHorizontalHeaderItem(4, new QStandardItem(QString("Time")));
+
     for(int i=0; i<lines.size(); i++){
-        QStandardItem *item = new QStandardItem(lines.at(i).toLocal8Bit().constData());
-        itemModel->appendRow(item);
+        QString line = QString(lines.at(i).toLocal8Bit().constData());
+        QStringList columns = line.split(" ", QString::SkipEmptyParts);
+
+        for(int j=0; j<5; j++){
+            QStandardItem *item = new QStandardItem(columns.at(j).toLocal8Bit().constData());
+            bootModel->setItem(i, j, item);
+        }
+
     }
+
+
+
+    ui->tableView->setModel(bootModel);
 
     ui->pushButton_2->setEnabled(false);
     ui->pushButton->setEnabled(true);
@@ -54,13 +72,39 @@ void MainWindow::on_pushButton_clicked()
     QStringList lines = stdout.split("\n", QString::SkipEmptyParts);
 
 
-    QStandardItemModel *bootModel = new QStandardItemModel(this);
-    for(int i=0; i<lines.size(); i++){
-        QStandardItem *item = new QStandardItem(lines.at(i).toLocal8Bit().constData());
-        bootModel->appendRow(item);
-    }
+    /*QStandardItemModel *bootModel = new QStandardItemModel(2,3,this); //2 Rows and 3 Columns
+    bootModel->setHorizontalHeaderItem(0, new QStandardItem(QString("Column1 Header")));
+    bootModel->setHorizontalHeaderItem(1, new QStandardItem(QString("Column2 Header")));
+    bootModel->setHorizontalHeaderItem(2, new QStandardItem(QString("Column3 Header")));*/
 
-    ui->listView1->setModel(bootModel);
+
+
+
+    QStandardItemModel *bootModel = new QStandardItemModel(30, 9, this);
+    bootModel->setHorizontalHeaderItem(0, new QStandardItem(QString("Boot No.")));
+    bootModel->setHorizontalHeaderItem(1, new QStandardItem(QString("Boot No.")));
+    bootModel->setHorizontalHeaderItem(2, new QStandardItem(QString("Boot No.")));
+    bootModel->setHorizontalHeaderItem(3, new QStandardItem(QString("Boot No.")));
+    bootModel->setHorizontalHeaderItem(4, new QStandardItem(QString("Boot No.")));
+    bootModel->setHorizontalHeaderItem(5, new QStandardItem(QString("Boot No.")));
+    bootModel->setHorizontalHeaderItem(6, new QStandardItem(QString("Boot No.")));
+    bootModel->setHorizontalHeaderItem(7, new QStandardItem(QString("Boot No.")));
+    bootModel->setHorizontalHeaderItem(8, new QStandardItem(QString("Boot No.")));
+
+   /* for(int i=0; i<lines.size(); i++){
+        QString line = QString(lines.at(i).toLocal8Bit().constData());
+        QStringList columns = line.split(" ");
+
+        for(int j=0; j<columns.size(); j++){
+            QStandardItem *item = new QStandardItem(columns.at(j));
+            bootModel->appendRow(item);
+        }
+
+    } */
+
+
+
+    ui->tableView->setModel(bootModel);
 
 }
 
