91a38c4145ee9160d2e033e37e7d392c836be9fe
Patrick Eigensatz <patrick.eigensatz@gmail.com>
2016-07-17 19:38:25 +0200

    Fixed autogen script

--- autogen.sh
+++ autogen.sh
@@ -3,9 +3,9 @@
 qmake journalctlgui.pro -r -spec linux-g++ CONFIG+=release
 
 if [ -d ".git" ]; then
-    sed -i -e 's/[[:xdigit:]]\{7\}/'$(git log --pretty=format:'%h' -n 1)'/' version.h
+    sed -i -e 's/[[:xdigit:]]\{7\}/'$(git log --pretty=format:'%h' -n 1)'/' src/version.h
 else
-    sed -i -e 's/[[:xdigit:]]\{7\}/0000000/' version.h
+    sed -i -e 's/[[:xdigit:]]\{7\}/0000000/' src/version.h
 fi
 
 
