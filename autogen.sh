54e09487d74db3814dae95dc022f90654e61f508
Patrick Eigensatz <patrick.eigensatz@gmail.com>
2019-11-18 19:48:26 +0100

    autogen.sh: Make sure to find correct qmake
    This fixes issue #38

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
-QT_SELECT=qt5 qmake qjournalctl.pro -r -spec linux-g++ CONFIG+=release
+# See Issue #38 -- Choose qmake first
+QMAKE_BIN="$(which qmake)"
+if [ ! -f "$QMAKE_BIN" ]; then
+    # -- If it's not there, try qmake-qt5 second
+    QMAKE_BIN="$(which qmake-qt5)"
+fi
+
+if [ ! -f "$QMAKE_BIN" ]; then
+    # If it's still not there...
+    echo "Did not find qmake. Please make sure either"
+    echo "qmake or qmake-qt5 is provided by your system."
+    exit 1
+fi
+
+QT_SELECT=qt5 $QMAKE_BIN qjournalctl.pro -r -spec linux-g++ CONFIG+=release
 
 if [ -d ".git" ]; then
     sed -i -e 's/[[:xdigit:]]\{7\}/'$(git log --pretty=format:'%h' -n 1)'/' src/version.h
