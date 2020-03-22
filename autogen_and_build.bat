c857a101b2c3de0fcde6ea80e42ebea2d020d5ae
Asier Lacasta <asierlacasta@gmail.com>
2020-03-22 17:42:19 +0100

    Uploading the autogen_and_build.bat script for the Qt Code generation, build and package of the qjournalctl applcation

---
+++ autogen_and_build.bat
@@ -0,0 +1,36 @@
+REM ---------------------------------------------------------------------
+REM Batch file to make all Makefiles or all Visual Studio project files
+REM ---------------------------------------------------------------------
+set OLD_PATH=%PATH%
+rem "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+rem set QTDIR=C:\Qt\Qt5.14.1\5.14.1\msvc2017
+rem "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+set QTDIR=C:\Qt\Qt5.14.1\5.14.1\msvc2017_64
+set PATH=%QTDIR%\bin;%PATH%
+set QMAKE_BIN=qmake
+set QT_SELECT=qt5
+set QMAKESPEC=win32-msvc
+
+rem 32 bits
+rem qmake qjournalctl.pro CONFIG+=release
+
+rem 64 bits
+qmake qjournalctl.pro CONFIG+=release CONFIG+=x86_64
+
+nmake
+windeployqt release/qjournalctl.exe
+
+rem 32 bits
+rem copy vcpkg\packages\libssh_x86-windows\bin\ssh.dll release\ssh.dll
+rem copy vcpkg\packages\pthreads_x86-windows\bin\ssh.dll release\pthreadVC3.dll
+
+rem 64 bits
+copy vcpkg\packages\libssh_x64-windows\bin\ssh.dll release\ssh.dll
+copy vcpkg\packages\pthreads_x64-windows\bin\pthreadVC3.dll release\pthreadVC3.dll
+
+del release\*.obj
+del release\*.cpp
+
+REM Restore the new path
+set PATH=%OLD_PATH%
+pause
