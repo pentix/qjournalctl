f3805cc6069692e2ce72b5974c2040f759d5ad97
Patrick Eigensatz <patrick.eigensatz@gmail.com>
2020-12-18 03:51:51 +0100

    Packaging: deb: Improved setup/scripts

--- packaging/deb/build_deb.sh
+++ packaging/deb/build_deb.sh
@@ -10,14 +10,15 @@ QT_SELECT=qt5 qmake /source/qjournalctl.pro -r -spec linux-g++ CONFIG+=release
 make -j$(nproc)
 
 # Move required files
-cp -r /source/packaging/files $OUT/
-mkdir -p $OUT/usr/bin
-mv qjournalctl $OUT/usr/bin
+cp -r /source/packaging/files "$OUT/"
+mkdir -p "$OUT/usr/bin"
+mv qjournalctl "$OUT/usr/bin"
 
 # debian pkg files
-mkdir -p $OUT/DEBIAN/
-cp /source/packaging/deb/control $OUT/DEBIAN/
-dpkg-deb --build $OUT
+mkdir -p "$OUT/DEBIAN/"
+cp /source/packaging/deb/control "$OUT/DEBIAN/"
+dpkg-deb --build "$OUT"
 
 # Keep ownership
-cat $OUT.deb > /out/$OUT.deb
+mv "$OUT.deb" "/out/$OUT.deb"
+chown $USERID:$GROUPID "/out/$OUT.deb"
